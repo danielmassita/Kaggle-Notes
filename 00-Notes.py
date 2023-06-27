99e75c95f95c516249840e568952dc9789d4cfa9
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-06-26 23:48:22 -0300

    Update 00-Notes.py

--- 00-Notes.py
+++ 00-Notes.py
@@ -1470,7 +1470,6 @@ def expert_finder(topic, client):
 
 
 
-
 # |----- Theory 07 -----|
 
 """
@@ -1862,5 +1861,104 @@ q_4.check()
 3	10855350.0
 4	10854542.0
 Correct
+"""
+
+
+
+
+
+# |----- Theory 08 -----|
+from google.cloud import bigquery
+
+# Create a "Client" object
+client = bigquery.Client()
+
+# Construct a reference to the "san_francisco" dataset
+dataset_ref = client.dataset("san_francisco", project="bigquery-public-data")
+
+# API request - fetch the dataset
+dataset = client.get_dataset(dataset_ref)
+
+# Construct a reference to the "bikeshare_trips" table
+table_ref = dataset_ref.table("bikeshare_trips")
+
+# API request - fetch the table
+table = client.get_table(table_ref)
+
+# Preview the first five lines of the table
+client.list_rows(table, max_results=5).to_dataframe()
+
+"""
+	trip_id	duration_sec	start_date	start_station_name	start_station_id	end_date	end_station_name	end_station_id	bike_number	zip_code	subscriber_type
+0	1235850	1540	2016-06-11 08:19:00+00:00	San Jose Diridon Caltrain Station	2	2016-06-11 08:45:00+00:00	San Jose Diridon Caltrain Station	2	124	15206	Customer
+1	1219337	6324	2016-05-29 12:49:00+00:00	San Jose Diridon Caltrain Station	2	2016-05-29 14:34:00+00:00	San Jose Diridon Caltrain Station	2	174	55416	Customer
+2	793762	115572	2015-06-04 09:22:00+00:00	San Jose Diridon Caltrain Station	2	2015-06-05 17:28:00+00:00	San Jose Diridon Caltrain Station	2	190	95391	Customer
+3	453845	54120	2014-09-15 16:53:00+00:00	San Jose Diridon Caltrain Station	2	2014-09-16 07:55:00+00:00	San Jose Diridon Caltrain Station	2	127	81	Customer
+4	1245113	5018	2016-06-17 20:08:00+00:00	San Jose Diridon Caltrain Station	2	2016-06-17 21:32:00+00:00	San Jose Diridon Caltrain Station	2	153	95070	Customer
+"""
+
+# Query to count the (cumulative) number of trips per day
+num_trips_query = """
+                  WITH trips_by_day AS
+                  (
+                  SELECT DATE(start_date) AS trip_date,
+                      COUNT(*) as num_trips
+                  FROM `bigquery-public-data.san_francisco.bikeshare_trips`
+                  WHERE EXTRACT(YEAR FROM start_date) = 2015
+                  GROUP BY trip_date
+                  )
+                  SELECT *,
+                      SUM(num_trips) 
+                          OVER (
+                               ORDER BY trip_date
+                               ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+                               ) AS cumulative_trips
+                      FROM trips_by_day
+                  """
+
+# Run the query, and return a pandas DataFrame
+num_trips_result = client.query(num_trips_query).result().to_dataframe()
+num_trips_result.head()
+"""
+	trip_date	num_trips	cumulative_trips
+0	2015-01-20	1213	16742
+1	2015-01-22	1224	19280
+2	2015-04-22	1376	108778
+3	2015-03-23	1247	76875
+4	2015-09-04	1129	248182
+"""
+
+# Query to track beginning and ending stations on October 25, 2015, for each bike
+start_end_query = """
+                  SELECT bike_number,
+                      TIME(start_date) AS trip_time,
+                      FIRST_VALUE(start_station_id)
+                          OVER (
+                               PARTITION BY bike_number
+                               ORDER BY start_date
+                               ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+                               ) AS first_station_id,
+                      LAST_VALUE(end_station_id)
+                          OVER (
+                               PARTITION BY bike_number
+                               ORDER BY start_date
+                               ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+                               ) AS last_station_id,
+                      start_station_id,
+                      end_station_id
+                  FROM `bigquery-public-data.san_francisco.bikeshare_trips`
+                  WHERE DATE(start_date) = '2015-10-25' 
+                  """
+
+# Run the query, and return a pandas DataFrame
+start_end_result = client.query(start_end_query).result().to_dataframe()
+start_end_result.head()
+"""
+	bike_number	trip_time	first_station_id	last_station_id	start_station_id	end_station_id
+0	294	14:29:00	64	77	64	77
+1	395	00:24:00	70	72	70	72
+2	480	15:56:00	39	67	39	67
+3	581	12:41:00	77	73	77	60
+4	581	12:56:00	77	73	60	73
 """
 
